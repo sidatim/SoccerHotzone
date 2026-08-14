74bd278f3c9a0df6f676aab0c2bd5ef111a0dc8a
sidatim <156040862+sidatim@users.noreply.github.com>
2026-08-14 17:28:27 -0600

    fixed heatmap pitch

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ from plotShots import plotGoals
 st.set_page_config(page_title="SoccerHotzone", page_icon=":soccer:")
 st.title("SoccerHotzone")
 players=[player["name"] for player in playerList]
+st.text("Ever wondered where your favorite soccer player scores their goals from? This app allows you to visualize the shot locations of top soccer players to see where they are most effective on the pitch.")
 selectedPlayer=st.selectbox("Select a player", players, key="selected_player", help="Select a player to view their shot data.", index=None)
-print(selectedPlayer)
+seasons=[f"{year}-{year+1}" for year in range(2010, 2025)]
+season=st.selectbox("Select a season", seasons, key="selected_season", help="Select a season to view the player's shot data.", index=None)
+filters=st.multiselect("Select filters", default=["Goals"], options=["Goals", "Shots on Target", "Non-Penalty Goals", "All Shots", "Preferred Foot", "Weak Foot"])
 if selectedPlayer:
-    fig, ax=plotGoals(selectedPlayer)
-    st.pyplot(fig)
+    playerData=next((player for player in playerList if player["name"]==selectedPlayer), None)
+    scatterFig, scatterAx, heatmapFig, heatmapAx=plotGoals(playerData, filters)
+    st.pyplot(scatterFig)
+    st.pyplot(heatmapAx.get_figure())
--- plotShots.py
+++ plotShots.py
@@ -3,19 +3,25 @@ import json
 import pandas as pd
 import streamlit as st
 st.cache_data(show_spinner=False,  ttl=None)
-def plotGoals(playerName):
-    with open(f"shotData/{playerName}_shots.json", "r", encoding="utf-8") as f:
+def plotGoals(player, filters):
+    with open(f"shotData/{player['name']}_shots.json", "r", encoding="utf-8") as f:
         shotData=json.load(f)
     shotData=pd.DataFrame(shotData)
     shotData["X"] = shotData["X"].astype(float) 
     shotData["Y"] = shotData["Y"].astype(float)
     goals=shotData[shotData["result"]=="Goal"]
     if goals.empty:
-        st.warning(f"No goals found for {playerName}")
+        st.warning(f"No goals found for {player['name']}")
         st.stop()
+
     pitch=mp.VerticalPitch(pitch_type='metricasports', pitch_width=68, pitch_length=100, pad_bottom=0.01, pitch_color='#22312b', line_color='#c7d5cc',  goal_type='box',stripe_color='#c7d5cc', half=True, axis=True)
-    fig, ax=pitch.draw(figsize=(10,6))
+    heatmapPitch=mp.VerticalPitch(pitch_type='metricasports', pitch_width=68, pitch_length=100, pad_bottom=0.01, pitch_color='#22312b', line_color='#c7d5cc',  goal_type='box',stripe_color='#c7d5cc', half=True, axis=True)
+    scatterFig, scatterAx=pitch.draw(figsize=(10,6))
+    heatmapFig, heatmapAx=pitch.draw(figsize=(10,6))
     x_plot = goals["X"]
     y_plot = 1 - goals["Y"]
-    pitch.scatter(x=x_plot, y=y_plot, s=20, c="red", edgecolors="black", alpha=0.7, ax=ax)
-    return fig, ax 
+    pitch.scatter(x=x_plot, y=y_plot, s=20, c="red", edgecolors="black", alpha=0.7, ax=scatterAx)
+    bin_statistic=heatmapPitch.bin_statistic(x=x_plot, y=y_plot, bins=(20, 10), statistic='count', normalize=True, )
+    heatmapPitch.heatmap(bin_statistic, cmap='Reds', edgecolors='#22312b', ax=heatmapAx, alpha=0.5)
+    labels=heatmapPitch.label_heatmap(bin_statistic, color='#22312b', fontsize=14, ax=heatmapAx, ha='center', va='center', str_format='{:.0%}', )
+    return scatterFig, scatterAx, heatmapFig, heatmapAx
